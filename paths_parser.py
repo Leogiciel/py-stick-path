bfbd1baf890778c77768ded937a800a6f1fe786e
Brice Andreota <brice@andreota.fr>
2021-03-15 11:54:19 +0100

    Raise exception on top and bottom values length

--- paths_parser.py
+++ paths_parser.py
@@ -25,11 +25,20 @@ def get_dimensions(line):
     return height, width
 
 
+def check_line(line, length):
+    if len(line) != length:
+        raise ValueError(f'Line [{line}] doesn\'t have required values length [{length}]')
+
+
 def get_paths(height, lines, width):
     paths = []
+    # standard format implies width % 3 == 1
     number_of_paths = int(1 + (width - 1) / 3)
+    # removing spaces to work on values ranks
     top_values_line = lines[1].replace(' ', '')
     bottom_values_line = lines[height].replace(' ', '')
+    check_line(top_values_line, number_of_paths)
+    check_line(bottom_values_line, number_of_paths)
     for i in range(number_of_paths):
         paths.append(Path(top_values_line[i], bottom_values_line[i]))
     return paths
